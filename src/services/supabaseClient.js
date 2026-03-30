2f159539f83eac4b6ccb50315511f70b5b2887d7
YASH RAJ <yaraj@gmail.com>
2026-03-30 18:23:51 +0530

    added env

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wbytrkhxehfzwwdxolnt.supabase.co";
-const supabaseAnonKey = "sb_publishable_35xPJt91tAHjZQ0Q8Ul-_g_tQzsjoR0";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
